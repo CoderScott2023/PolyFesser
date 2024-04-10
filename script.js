74e6e5dca3b3d57c92caa2be37e165185c39a6b3
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2024-04-10 08:00:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -45,8 +45,8 @@ function onClick()
       var tribe2 = document.getElementById("tribe2").value;
       var filters = {};
       const winRate = calculateWinRate(data, tribe1, tribe2, filters);
-      document.write(`Win rate for ${tribe1} vs ${tribe2} with filters: ${winRate}%`);
+      console.log(`Win rate for ${tribe1} vs ${tribe2} with filters: ${winRate}%`);
     })
-    .catch(error => document.write('Error fetching game data: Problem with calculateWinRate function', error));
+    .catch(error => console.log('Error fetching game data: Problem with calculateWinRate function', error));
 }
 
