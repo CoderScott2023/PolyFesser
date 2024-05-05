a4f1b215c9d1a4b6d48669f0aab7eca4bf427c97
Scott Vars <142948724+CoderScott2023@users.noreply.github.com>
2024-05-05 07:03:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -46,7 +46,7 @@ function loadGameData(fileName) {
 }
 
 function onClick() {
-  loadGameData('polyelo_data.json') // Replace with your actual file path
+  loadGameData('polyelo_data.json')
     .then(data => {
       // Game data loaded successfully
       var tribe1 = document.getElementById("tribe1").value;
